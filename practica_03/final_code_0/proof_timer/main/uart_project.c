bb1db9c4b4f814b3c96606961a5b799f4613659b
David-Alfonso <davidsantiagoam@gmail.com>
2024-05-10 21:51:16 -0500

    finalFinalFinal

---
+++ practica_03/final_code_0/proof_timer/main/uart_project.c
@@ -0,0 +1,89 @@
+/* uart_project.c - Implementación del módulo de uart */
+
+#include "uart_project.h"
+
+/* Configura la UART0 */
+void uart0_config(uint32_t baudrate, int tx_pin, int rx_pin){
+
+    // Desactivar temporalmente la UART
+    UART0.conf0.val = 0;
+
+    // Configurar el baud rate
+    uint32_t parteEntera = (uint32_t) (CLKSRC / baudrate);
+    uint32_t parteDecimal = (uint32_t)(((CLKSRC / baudrate) - parteEntera) * 10);
+    parteEntera = parteEntera & UART_CLKDIV_V;
+    parteDecimal = parteDecimal & UART_CLKDIV_FRAG_V;
+    UART0.clk_div.val = (parteEntera | (parteDecimal << UART_CLKDIV_FRAG_S));
+
+    UART0.mem_conf.rx_size = 15;
+
+    // Configurar los bits de datos, paridad y bits de parada
+    UART0.conf0.val |= (1 << UART_TICK_REF_ALWAYS_ON_S)
+                    | (3 << UART_BIT_NUM_S)                // 8 bits de datos
+                    | (0 << UART_PARITY_EN_S)              // Paridad deshabilitada
+                    | (1 << UART_STOP_BIT_NUM_S);          // 1 bit de parada
+
+    uart_set_pin(UART_NUM_0, tx_pin, rx_pin, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE);
+}
+
+/* Envía un byte usando UART0 */
+void uart0_send_byte(uint8_t data) {
+    UART0.fifo.rw_byte = data; // Escribir el byte en el FIFO
+}
+
+/* Recibe un byte usando UART0 */
+uint8_t uart0_receive_byte(uint8_t *var) {
+    if((UART0.status.rxfifo_cnt > 0) || (UART0.mem_cnt_status.rx_cnt > 0))
+    {
+        *var = (uint8_t)(UART0.fifo.rw_byte);
+        return 1;
+    }
+    return 0;
+}
+
+/* Envia una cadena de caracteres usando UART0 */
+void uart0_send_string(const char* str) {
+    uart0_send_byte((uint8_t)*str++);
+}
+
+/* Devuelve numero bytes almacenados en RXFIFO */
+uint8_t get_uart0_rxfifo_cnt (void){
+    return UART0.status.rxfifo_cnt;
+}
+
+/* Devuelve numero bytes almacenados en TXFIFO */
+uint8_t get_uart0_txfifo_cnt (void){
+    return UART0.status.txfifo_cnt;
+}
+
+/* Flush a TX FIFO */
+void reset_uart0_txfifo (void){
+    UART0.conf0.txfifo_rst = UART_TXFIFO_RST_V;
+    UART0.conf0.txfifo_rst = 0;
+}
+
+
+/* Flush a RX FIFO */
+void reset_uart0_rxfifo (void){
+    UART0.conf0.rxfifo_rst = UART_RXFIFO_RST_V;
+    UART0.conf0.rxfifo_rst = 0;
+}
+
+/* Return 1 if has something and 0 if not */
+uint8_t is_not_empty_uart0(void){
+    if((UART0.status.rxfifo_cnt) || (UART0.mem_cnt_status.rx_cnt))
+    {
+        return 1;
+    }
+    return 0;
+}
+
+/* vacia buffer de rx */
+void vaciar_buffer_rx()
+{
+    uint8_t valor = 0;
+    for(int i = 0; i < 128; i++)
+    {
+        valor = UART0.fifo.rw_byte;
+    }
+}
